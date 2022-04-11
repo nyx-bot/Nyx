50955c8e5ff5274d6869a0c64a1b59ea0983a924
Syl <syl@nyxbot.xyz>
2022-04-11 09:05:30 -0500

    totally code cleanliness and not a test

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ const createDiscordClient = async () => new Promise(async res => {
             interactionCache[interaction.id] = true;
 
             const isCommand = interaction.isCommand() ? `COMMAND` : null;
-            const isButton = (interaction.isButton() || interaction.isSelectMenu()) ? `BUTTON` : null;
+            const isButton = interaction.isButton() || interaction.isSelectMenu() ? `BUTTON` : null;
 
             let log = `[${(isCommand ? isCommand + ` ` : ``) + (isButton ? isButton + ` ` : ``) + interaction.type.toUpperCase()} INTERACTION]`;
             if(interaction.guild) log = log + `\n| Guild ID: ${interaction.guild.id || interaction.guildId}`;

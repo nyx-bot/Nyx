ea9fa5e9f24d0cdab8b59507c84b053066bedad9
Syl <hi@sylviiu.dev>
2022-07-11 00:53:56 -0400

    set up multilingual support for 8ball command (english + spanish as of right now)

--- core/commandHandler/cmdModules/fun/8ball.js
+++ core/commandHandler/cmdModules/fun/8ball.js
@@ -1,28 +1,5 @@
-const responses = [
-    "yup.", 
-    "i mean, it could happen.", 
-    "there's a chance", 
-    "try harder.",  
-    "of course!", 
-    "mayhaps", 
-    "possibly", 
-    "nope", 
-    "give a better question than that", 
-    "can't hear u, i'm blasting naku's music", 
-    "sure, why not", 
-    "lmao nah", 
-    "i mean it'd be cool if it were possible", 
-    "weird question, but go off sister", 
-    "maybe in another timeline", 
-    "ask again never", 
-    "i really don't know how to respond", 
-    "probably not, but off topic; have you heard of megalovania?", 
-    "this is a wendy's drive through", 
-    "no."
-];
-
 const func = (interaction) => interaction.reply({
-    content: responses[Math.floor((Math.random() * responses.length))]
+    content: interaction.langFile[Math.floor((Math.random() * interaction.langFile.length))]
 });
 
 module.exports = {
---
+++ lang/en-US/fun/8ball.js
@@ -0,0 +1,22 @@
+module.exports = [
+    "yup.", 
+    "i mean, it could happen.", 
+    "there's a chance", 
+    "try harder.",  
+    "of course!", 
+    "mayhaps", 
+    "possibly", 
+    "nope", 
+    "give a better question than that", 
+    "can't hear u, i'm blasting naku's music", 
+    "sure, why not", 
+    "lmao nah", 
+    "i mean it'd be cool if it were possible", 
+    "weird question, but go off sister", 
+    "maybe in another timeline", 
+    "ask again never", 
+    "i really don't know how to respond", 
+    "probably not, but off topic; have you heard of megalovania?", 
+    "this is a wendy's drive through", 
+    "no."
+]

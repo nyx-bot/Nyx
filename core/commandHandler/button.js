2b0443dd6da6cc4f072de37c8f144a19e64a1fea
Syl <hi@sylviiu.dev>
2022-07-27 11:22:38 -0500

    check if language file exists for certain command, attach to interaction object if so

--- core/commandHandler/button.js
+++ core/commandHandler/button.js
@@ -1,8 +1,36 @@
+const fs = require('fs')
+
 module.exports = interaction => new Promise(async res => {
     if((ctx.ready || ctx.config.elevated.find(i => i == interaction.user.id)) && ctx.bot.guilds.cache.has(interaction.guildId)) {
         if(global.ctx.cmdModuleMap.has(interaction.message.interaction.commandName)) {
             const command = global.ctx.modules[global.ctx.cmdModuleMap.get(interaction.message.interaction.commandName.toLowerCase())].commands[interaction.message.interaction.commandName.toLowerCase()];
-            res(command)
+            res(command);
+
+            const file = `${global.ctx.cmdModuleMap.get(interaction.message.interaction.commandName.toLowerCase()).toLowerCase()}/${interaction.message.interaction.commandName.toLowerCase()}`
+
+            const preferredLanguage = `./lang/${interaction.locale}/${file}.js`;
+            const defaultLanguage = `./lang/${require(`../../config.json`).defaultLanguage}/${file}.js`;
+
+            if(fs.existsSync(preferredLanguage)) {
+                console.log(`Locale ${interaction.locale} file exists for ${file}`);
+                try {
+                    interaction.langFile = require(`../.${preferredLanguage}`)
+                } catch(e) {
+                    console.log(`Unable to add preferred langFile for ${file}: ${e}`)
+                }
+            };
+            
+            if(!interaction.langFile && fs.existsSync(defaultLanguage)) {
+                console.log(`Locale ${interaction.locale} file does NOT exist for ${file} -- however, default language (${require(`../../config.json`).defaultLanguage}) exists.`)
+                try {
+                    interaction.langFile = require(`../.${defaultLanguage}`)
+                } catch(e) {
+                    console.log(`Unable to add default langFile for ${file}: ${e}`)
+                }
+            };
+            
+            if(!interaction.langFile) console.log(`There was no usable locale file for ${file} (tried user's & default)`)
+
             try {
                 command.buttonFunc(interaction)
             } catch(e) {

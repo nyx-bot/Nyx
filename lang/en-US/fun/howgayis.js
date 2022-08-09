7c0f2ab42847b5851f4d3bd6a055f05251a23a71
Syl <hi@sylviiu.dev>
2022-08-09 16:49:36 -0500

    remove translations from nyx main repo

--- lang/en-US/fun/howgayis.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    bot: `I'm pretty sure most bots don't even know the definition of that...`,
-
-    measure: (username, percentNumber) => `**${username}** is ${percentNumber}% gay ${(Number(percentNumber) >= 80 ? `... ${ctx.emojis.nyx.smirk}` : ``)}`
-}
--- lang/es-ES/fun/howgayis.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    bot: `Creo que la mayoría de los bots no saben qué es eso...`,
-
-    measure: (username, percentNumber) => `**${username}** es ${percentNumber}% gay ${(Number(percentNumber) >= 80 ? `... ${ctx.emojis.nyx.smirk}` : ``)}`
-}

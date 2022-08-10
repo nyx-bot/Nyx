79f753e4c6519f81835c0e916d476194ca90787f
Syl <hi@sylviiu.dev>
2022-08-09 20:38:43 -0500

    Urban Dictionary translations

--- core/commandHandler/cmdModules/fun/urban.js
+++ core/commandHandler/cmdModules/fun/urban.js
@@ -10,49 +10,47 @@ const findResponses = (interaction, query, n, deferReplyFunc, defer, func) => ne
             let term = r.body.list[n]
             var trimmedfafa = false;
             var length = 512;var trimmed = (`${term.definition}`).substring(0, length);var definition;if(trimmed !== term.definition) {trimmedfafa = true; definition = `${trimmed}`} else {definition = term.definition}
-            if(trimmedfafa) {definition = `${definition}... <bg>Continue Reading<end>(${term.permalink})`}
+            if(trimmedfafa) {definition = `${definition}... <bg>${interaction.langFile.responses.main.continue}<end>(${term.permalink})`}
             var trimmedfafa = false;
             var trimmedB = (`${term.example}`).substring(0, length);var example;if(trimmedB !== term.example) {trimmedfafa = true; example = `${trimmedB}`} else {example = trimmed}
-            if(trimmedfafa) {example = `${example}... <bg>Continue Reading<end>(${term.permalink})`}
+            if(trimmedfafa) {example = `${example}... <bg>${interaction.langFile.responses.main.continue}<end>(${term.permalink})`}
             
             let embed = new ctx.libs.builder.EmbedBuilder()
             .setTitle(`Urban Dictionary: **${term.word}** [${n+1}/${r.body.list.length}]`)
             .setColor(ctx.utils.colors('random'))
-            .setDescription(`\n> ${(definition).replace(/\[/g, '**').replace(/\]/g, '**').replace(/<br>/g, '\n').replace(/\n/g, '> ').replace(/<bg>/g, '[').replace(/<end>/g, ']')}\n\n**Example(s)**\n${(trimmedB).replace(/\[/g, '**').replace(/\]/g, '**')}\n\n[__**View on Urban Dictionary**__](${term.permalink})`)
-            .setFooter({ text: `👍 ${term.thumbs_up} 👎 ${term.thumbs_down}   |   Author: ${term.author}` });
+            .setDescription(`\n> ${(definition).replace(/\[/g, '**').replace(/\]/g, '**').replace(/<br>/g, '\n').replace(/\n/g, '> ').replace(/<bg>/g, '[').replace(/<end>/g, ']')}\n\n**${interaction.langFile.responses.main.examples}**\n${(trimmedB).replace(/\[/g, '**').replace(/\]/g, '**')}\n\n[__**${interaction.langFile.responses.main.view}**__](${term.permalink})`)
+            .setFooter({ text: `👍 ${term.thumbs_up} 👎 ${term.thumbs_down}   |   ${interaction.langFile.responses.main.author}: ${term.author}` });
 
             const buttons = [];
         
             buttons.push(
                 new ctx.libs.builder.MessageButton()
                 .setCustomId(`previous`)
-                .setLabel(`◀ ` + `Previous Entry`)
-                .setStyle(`SECONDARY`)
+                .setLabel(`◀ ` + `${interaction.langFile.responses.buttons.previous}`)
+                .setStyle(`Secondary`)
                 .setDisabled(r.body.list[n-1] ? false : true)
             )
         
             buttons.push(
                 new ctx.libs.builder.MessageButton()
                 .setCustomId(`next`)
-                .setLabel(`Next Entry` + ` ▶`)
-                .setStyle(`PRIMARY`)
+                .setLabel(`${interaction.langFile.responses.buttons.next}` + ` ▶`)
+                .setStyle(`Primary`)
                 .setDisabled(r.body.list[n+1] ? false : true)
             )
         
             return res(interaction[funcProp]({
-                content: `${ctx.emojis.nyx.happy} I found ${r.body.list.length} result${r.body.list.length.length === 1 ? `` : `s`} for **${ctx.utils.escape(query)}**`,
+                content: `${ctx.emojis.nyx.happy} ` + (r.body.list.length === 1 ? interaction.langFile.responses.messages[`one-result`] : interaction.langFile.responses.messages[`more-than-one-result`]).replace(/%num%/g, r.body.list.length).replace(/%query%/g, ctx.utils.escape(query)),
+                //content: `${ctx.emojis.nyx.happy} I found ${r.body.list.length} result${r.body.list.length.length === 1 ? `` : `s`} for **${ctx.utils.escape(query)}**`,
                 embeds: [embed.toJSON()],
                 components: [new ctx.libs.builder.MessageActionRow().addComponents(...buttons)],
             }))
-        } else return res(interaction.editReply(`${ctx.emojis.fail} There is no definition of **${ctx.utils.escape(decodeURI(termA).replace(/%20/g, ' '))}**!`))
+        } else return res(interaction.editReply(`${ctx.emojis.fail} ${interaction.langFile.responses.messages[`no-definition`].replace(/%query%/g, ctx.utils.escape(decodeURI(termA).replace(/%20/g, ' ')))}`))
+        //} else return res(interaction.editReply(`${ctx.emojis.fail} There is no definition of **${ctx.utils.escape(decodeURI(termA).replace(/%20/g, ' '))}**!`))
     };
 
     const errHandler = (err, interaction) => {
         let message = null;
-    
-        switch (err.status) {
-            case 400: message = `I was unable to properly request the dictionary entry!`
-        }
     
         return res(interaction.editReply(ctx.errMsg(interaction, message ? `${message} [${err.status}]` : null, `Error handling urban dictionary request`, err)))
     };
@@ -78,7 +76,7 @@ const buttonFunc = async interaction => {
     const clicked = interaction.component.customId;
 
     try {
-        const query = interaction.message.content.slice(56, -2).replace(/\\/, ``);
+        const query = interaction.message.embeds[0].title.slice(20).split(`[`)[0].slice(0, -3)
         const num = Number(interaction.message.embeds[0].title.split(` `).reverse()[0].slice(1, -1).split(`/`)[0])-1;
 
         console.log(`Query: ${query}\n| New index: ${num}`)
@@ -94,14 +92,20 @@ const func = interaction => {
 module.exports = {
     func,
     buttonFunc,
-    interaction: new (require('@discordjs/builders').SlashCommandBuilder)()
-        .setDescription(`Search a term on the worst dictionary :)`)
-        .setDefaultPermission(true)
-        .addStringOption(s => {
-            s.setName(`query`);
-            s.setDescription(`The term you want to look up`)
-            s.setRequired(true);
-
-            return s;
-        })
+    interactionOptions: [
+        {
+            type: `string`,
+            requred: true
+        }
+    ]
+    //interaction: new (require('@discordjs/builders').SlashCommandBuilder)()
+    //    .setDescription(`Search a term on the worst dictionary :)`)
+    //    .setDefaultPermission(true)
+    //    .addStringOption(s => {
+    //        s.setName(`query`);
+    //        s.setDescription(`The term you want to look up`)
+    //        s.setRequired(true);
+//
+    //        return s;
+    //    })
 }

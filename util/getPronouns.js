be61f24d7a9aefae39a4f9769b9a0c83324782b4
Syl <hi@sylviiu.dev>
2022-11-04 11:39:10 -0500

    remove getPronouns func (adding later when db is established)

--- util/getPronouns.js
+++
@@ -1,8 +0,0 @@
-module.exports = id => new Promise(async res => {
-    if(typeof id == `object` && id.id && typeof id.id == `string`) {id = id.id};
-
-    const fallback = ctx.seq.models.userPronouns.build({id: `${id}`});
-    const g = await ctx.seq.models.userPronouns.findOne({where: {id: `${id}`}});
-
-    return res(g || fallback)
-})

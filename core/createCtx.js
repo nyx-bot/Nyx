8e7ddc31379020b0bb4860894d2b99bc427942f4
Syl <hi@sylviiu.dev>
2022-11-04 11:37:33 -0500

    rename initCtx to createCtx for better understanding

--- core/createCtx.js
+++ core/createCtx.js
@@ -9,7 +9,7 @@ module.exports = () => {
         const name = file.split(`.`).slice(0, -1).join(`.`)
         try {
             ctx[name] = require(`./ctxModules/${file}`);
-            console.log(`[CTX] Added ${name} (${typeof ctx[name]})`)
+            console.debug(`[CTX] Added ${name} (${typeof ctx[name]})`)
         } catch(e) {
             console.warn(`[CTX] Failed to load ctx module ${file}! (${e})`)
         }

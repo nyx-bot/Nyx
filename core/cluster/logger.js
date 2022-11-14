cd8925ee4791b5739d7ffd561909439b6b6b3aa8
Syl <hi@sylviiu.dev>
2022-11-14 13:31:46 -0600

    make modified console.log accessible at the level of the cluster manager as well as client

--- core/cluster/logger.js
+++ core/cluster/logger.js
@@ -1,3 +1,5 @@
+const debug = process.argv.find(a => a.toLowerCase() == `debug`) ? true : false
+
 const c = {
     reset: "\x1b[0m", bold: "\x1b[1m", italic: "\x1b[3m", underline: "\x1b[4m", strike: "\x1b[9m", clearline: "\x1b[K",
     black: "\x1b[30m", red: "\x1b[31m", green: "\x1b[32m", yellow: "\x1b[33m", blue: "\x1b[34m", magenta: "\x1b[35m", cyan: "\x1b[36m", white: "\x1b[37m",
@@ -27,14 +29,23 @@ let logChannels = {};
 types.forEach(rawtype => {
     let type = rawtype[0], colors = rawtype.slice(1)
 
-    logChannels[type] = (c, ...m) => m.forEach(msg => {
+    if(console[type]) console[`original${type}`] = console[type]
+    if(console[type[0]]) console[`original${type[0]}`] = console[type[0]]
+
+    const func = (c, ...m) => m.forEach(msg => {
         try {
             let thisTag = tag(type, colors, c);
-            console.log(`${thisTag} ` + (typeof msg == `string` ? msg : require("util").inspect(msg, { depth: 0 })).split(`\n`).join(`\n${thisTag} `))
+            console[`original${type}`](`${thisTag} ` + (typeof msg == `string` ? msg : require("util").inspect(msg, { depth: 0 })).split(`\n`).join(`\n${thisTag} `))
         } catch(e) {
-            console.error(e)
+            console[`originalerror`](e)
         }
-    })
+    });
+
+    logChannels[type] = func
+    console[type] = (...d) => func(`--`, ...d);
+    console[type[0]] = (...d) => func(`--`, ...d);
+
+    if(!debug) logChannels[`debug`] = () => {}
 });
 
 module.exports = logChannels
--- index.js
+++ index.js
@@ -8,27 +8,35 @@ global.manager = new ClusterManager(`./client.js`, {
     token: config.token,
 });
 
-const logger = require('./manager/logger');
+const logger = require('./core/cluster/logger')
 
-const log = (...d) => logger.main(`--`, ...d);
-
-manager.on(`clusterCreate`, async cluster => {
-    log(`Launched cluster ${Number(cluster.id)+1}`);
-
-    cluster.on(`message`, opt => {
-        try {
-            if(logger[opt.name] && ((opt.name == `debug` && debug) || opt.name != `debug`)) {
-                logger[opt.name](Number(cluster.id)+1, opt.msg)
+require('./core/cluster/createSlashCommands')(logger).then(async cmds => {
+    manager.on(`clusterCreate`, async cluster => {
+        console.log(`Launched cluster ${Number(cluster.id)+1}`);
+    
+        cluster.on(`error`, e => {
+            console.error(`--`, e)
+        })
+    
+        cluster.on(`message`, opt => {
+            try {
+                if(logger[opt.name]) {
+                    logger[opt.name](Number(cluster.id)+1, opt.msg)
+                }
+            } catch(e) {console.error(e)}
+        })
+    
+        cluster.on(`ready`, async () => {
+            console.log(`Cluster ${Number(cluster.id)+1}/${manager.totalClusters} is ready!`);
+            if(Number(cluster.id)+1 == manager.totalClusters) {
+                console.log(`| Nyx is ready!\n| Serving ${(await (require('./util/getTotalServerCount')()))} guilds across ${manager.totalShards} shards & ${manager.totalClusters} clusters`)
             }
-        } catch(e) {console.error(e)}
-    })
-
-    cluster.on(`ready`, async () => {
-        log(`Cluster ${Number(cluster.id)+1}/${manager.totalClusters} is ready!`);
-        if(Number(cluster.id)+1 == manager.totalClusters) {
-            log(`| Nyx is ready!\n| Serving ${(await (require('./util/getTotalServerCount')()))} guilds across ${manager.totalShards} shards & ${manager.totalClusters} clusters`)
-        }
-    })
-});
-
-manager.spawn();
+        })
+    });
+    
+    try {
+        manager.spawn();
+    } catch(e) {
+        console.error(`--`, `Failed to spawn cluster: ${e}`);
+    }
+})

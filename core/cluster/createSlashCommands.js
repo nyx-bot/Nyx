f45de0326cdf95395361ff027f1f2f21911d6312
Syl <hi@sylviiu.dev>
2022-11-14 13:37:01 -0600

    reflect required parameter for slash command generation

--- core/cluster/createSlashCommands.js
+++ core/cluster/createSlashCommands.js
@@ -25,23 +25,23 @@ module.exports = () => new Promise(async res => {
                 console.debug(`Parsing command ${cmdName}:`)
 
                 slashCommand.setName(command.languages[`en-US`].command.name);
-                console.debug(`- Set name as "${command.languages[`en-US`].command.name}"`);
+                console.debug(`| Set name as "${command.languages[`en-US`].command.name}"`);
 
                 let nameLocales = {}; 
                 Object.entries(command.languages).forEach(o => nameLocales[o[0]] = o[1].command.name);
                 slashCommand.setNameLocalizations(nameLocales);
-                console.debug(`- Applied ${Object.keys(nameLocales).length} localization(s) for name`)
+                console.debug(`| Applied ${Object.keys(nameLocales).length} localization(s) for name`)
 
                 slashCommand.setDescription(command.languages[`en-US`].command.description);
-                console.debug(`- Set description as "${command.languages[`en-US`].command.description}"`)
+                console.debug(`| Set description as "${command.languages[`en-US`].command.description}"`)
 
                 let descriptionLocales = {}; 
                 Object.entries(command.languages).forEach(o => descriptionLocales[o[0]] = o[1].command.description);
                 slashCommand.setDescriptionLocalizations(descriptionLocales);
-                console.debug(`- Applied ${Object.keys(descriptionLocales).length} localization(s) for description`);
+                console.debug(`| Applied ${Object.keys(descriptionLocales).length} localization(s) for description`);
 
                 if(command.languages[`en-US`].command.options) {
-                    console.log(`${command.languages[`en-US`].command.options.length} option(s) exist!`);
+                    console.debug(`| ${command.languages[`en-US`].command.options.length} option(s) exist!`);
 
                     for(i in command.languages[`en-US`].command.options) {
                         const opt = command.languages[`en-US`].command.options[i];
@@ -51,24 +51,29 @@ module.exports = () => new Promise(async res => {
                             func = `add${opt.type[0].toUpperCase + opt.type.slice(1)}Option`;
                         };
 
-                        console.debug(`- Adding ${func.slice(3, -6)} option`)
+                        console.debug(`| Adding ${func.slice(3, -6)} option`)
 
                         slashCommand[func](interaction => {
                             interaction.setName(opt.name);
-                            console.debug(`- - Set ${func.slice(3, -6)} option name as ${opt.name}`)
+                            console.debug(`| - Set ${func.slice(3, -6)} option name as ${opt.name}`)
 
                             let slashNameLocales = {}; 
                             Object.entries(command.languages).forEach(o => slashNameLocales[o[0]] = o[1].command.options[i].name);
                             interaction.setNameLocalizations(slashNameLocales);
-                            console.debug(`- - Applied ${Object.keys(slashNameLocales).length} localization(s) for name`)
+                            console.debug(`| - Applied ${Object.keys(slashNameLocales).length} localization(s) for name`)
 
                             interaction.setDescription(opt.description);
-                            console.debug(`- - Set ${func.slice(3, -6)} option description as ${opt.description}`)
+                            console.debug(`| - Set ${func.slice(3, -6)} option description as ${opt.description}`)
 
                             let slashDescriptionLocales = {}; 
                             Object.entries(command.languages).forEach(o => slashDescriptionLocales[o[0]] = o[1].command.options[i].description);
                             interaction.setDescriptionLocalizations(slashDescriptionLocales);
-                            console.debug(`- - Applied ${Object.keys(slashDescriptionLocales).length} localization(s) for description`);
+                            console.debug(`| - Applied ${Object.keys(slashDescriptionLocales).length} localization(s) for description`);
+
+                            if(opt.required) {
+                                console.debug(`| - ${func.slice(3, -6)} option is required!`);
+                                interaction.setRequired(true);
+                            }
 
                             return interaction;
                         })

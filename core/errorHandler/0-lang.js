45b43f7ca00db7e99d7432df5255c5c0d8f0ed1c
Syl <hi@sylviiu.dev>
2022-11-18 12:28:58 -0600

    initial language file error handling

--- core/errorHandler/0-lang.js
+++ core/errorHandler/0-lang.js
@@ -1,7 +1,7 @@
 module.exports = {
     find: (e) => `${e}`.toLowerCase().includes(`cannot find module`) && `${e}`.includes(`/lang/`),
     func: (e, msgs) => {
-        const name = `${e}`.split(`/lang/`).join(`/`).split(` `)[0]
+        const name = (`${e}`.split(`/`).slice(1).join(`/`).split(` `)[0].split(`'`)[0]).split(`lang/`)[1]
         if(require('fs').readdirSync(`./lang/`).length > 0) {
             console.error(`Failed to find lang file ${name}\n| Please make sure Nyx's language files are up to date with "git submodule update --remote" (Language files exist, just update them!)\n| ${msgs.openIssue}`)
         } else {
